f55773037cabdfe64518f2d3fadaad0582bf9f10
dot-Sh3ll <paulmoussard@gmail.com>
2019-11-26 00:58:41 +0100

    no message

--- react16_reactrouter4_firebase/premierprojet/src/App.js
+++ react16_reactrouter4_firebase/premierprojet/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { Component, Fragment } from 'react'
 import './App.css'
+import Membre from './components/Membre'
 
-function App () {
-  return (
-    <div className='App'>
-      <h1>React app</h1>
-    </div>
-  )
+class App extends Component {
+  render () {
+    return (
+      <Fragment>
+        <div className='App'>
+          <p className='test'>voila</p>
+          <h1>React app</h1>
+          <Membre />
+        </div>
+        <h2>Mon second titre</h2>
+      </Fragment>
+    )
+  }
 }
 
 export default App
---
+++ react16_reactrouter4_firebase/premierprojet/src/components/Membre.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Membre = () => (
+        <h2>Membre de ma famille</h2>
+    )
+
+export default Membre
