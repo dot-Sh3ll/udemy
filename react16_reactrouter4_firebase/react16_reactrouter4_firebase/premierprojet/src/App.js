5b03ecf7517e8ddd4bdc70319a0ade13e380e8b3
dot-Sh3ll <paulmoussard@gmail.com>
2019-11-26 01:17:11 +0100

    remise à zero

---
+++ react16_reactrouter4_firebase/react16_reactrouter4_firebase/premierprojet/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react'
+import './App.css'
+import Membre from './components/Membre'
+
+class App extends Component {
+  render () {
+    return (
+      <Fragment>
+        <div className='App'>
+          <p className='test'>voila</p>
+          <h1>React app</h1>
+          <Membre />
+        </div>
+        <h2>Mon second titre</h2>
+      </Fragment>
+    )
+  }
+}
+
+export default App
+
+
