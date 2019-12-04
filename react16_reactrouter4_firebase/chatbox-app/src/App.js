a5b7753dcfd3b670230834e78e3868ede7861b0c
dot-Sh3ll <paulmoussard@gmail.com>
2019-12-04 11:16:47 +0100

    petit save

--- react16_reactrouter4_firebase/chatbox-app/src/App.js
+++ react16_reactrouter4_firebase/chatbox-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 import './App.css'
 
 import Formulaire from './components/Formulaire'
@@ -14,16 +14,34 @@ class App extends Component {
     pseudo: this.props.match.params.pseudo
   }
 
+  // importation de createRef() pour la faire vivre dans l'application (permet la manipulation du dom)
+  messagesRef = createRef()
+
   componentDidMount () {
+    // synchronise l'app avec firebase pour récupérer les derniers messages
     base.syncState('/', {
       context: this,
       state: 'messages'
     })
   }
 
+  componentDidUpdate () {
+    // dans le chat, permet de scroller automatiquement en bas des derniers messages
+    const ref = this.messagesRef.current
+    ref.scrollTop = ref.scrollHeight
+  }
+
   addMessage = message => {
     const messages = { ...this.state.messages }
     messages[`message-${Date.now()}`] = message
+    // supprime les messages quand le chat dépasse 10 messages
+    Object
+      // on boucle sur messages en récupérant les keys
+      .keys(messages)
+      // on met de coté les 10 derniers messages pour ne pas les toucher
+      .slice(0, -10)
+      // pour chaque messages qui restent, on leur donne la valeur null
+      .forEach(key => (messages[key] = null))
     this.setState({ messages })
   }
   render () {
@@ -42,7 +60,7 @@ class App extends Component {
     return (
       <div className='box'>
         <div>
-          <div className='messages'>
+          <div className='messages' ref={this.messagesRef}>
             <div className='message'>{messages}</div>
           </div>
         </div>
